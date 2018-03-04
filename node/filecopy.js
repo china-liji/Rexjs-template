c097448f1d46990948b82992903ede1dfbd9d8eb
china-liji <hello_zaizi@163.com>
2018-03-05 00:22:56 +0800

    根据最新 rexjs 依赖

--- node/filecopy.js
+++ node/filecopy.js
@@ -3,5 +3,5 @@ let path = require("path"), libPath = path.resolve(__dirname, "../test");
 new (
 	require("rexjs-filecopy")
 )({
-	"rexjs-api/rex.bundle.js": libPath
+	"rexjs-api/dist/rex.bundle.js": libPath
 });
